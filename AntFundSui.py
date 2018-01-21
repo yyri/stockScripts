d06c27fe410b406a76d89b28902b3abb1493392b
Yancy <yue.yyri@gmail.com>
2018-01-21 11:36:38 +0800

    Update AntFundSui.py

--- AntFundSui.py
+++ AntFundSui.py
@@ -17,9 +17,9 @@ from datetime import datetime
 
 print("Python3 is expected.Current Version is:" + sys.version)
 
-jsessionid = '3770EAA80D813701D2B0463993A473BE'
-sessionCookie = '14ef1450ea7d6f4f82ef548c622dde6d'
-filename = "C:/downloads/alipay_record_20180120_2135/alipay_record_20180120_2135_1.csv"
+jsessionid = ''
+sessionCookie = ''
+filename = "C:/alipay_record_2135_1.csv"
 
 # https://consumeprod.alipay.com/record/standard.htm  高级筛选，搜索“蚂蚁财富”
 with open(filename) as csv_file:
@@ -62,18 +62,8 @@ with open(filename) as csv_file:
             # postdata = params.encode('utf-8')
 
             cookies = {
-                # '__vistor': 'DBFAC5281c1lhem71',
-                # 'Hm_lvt_3db4e52bb5797afe0faaa2fde5c96ea4': '1515662855,1515726830,1515728528,1515813996',
-                # '__nick': 'yyri"%"40163.com',
-                # '_bookTabSwitchList': 'f8bb35fce3a08671b3ae2ed6c33e69c8|0|0&eaf0f8eee8ac43901d493221d4db8cd0|0|0&',
-                # '__utma': '121176714.950744407.1513591643.1516352533.1516414389.48',
-                # '__utmz': '121176714.1514086705.5.3.utmcsr=lc.ssjlicai.com|utmccn=(referral)|utmcmd=referral|utmcct=/index',
                 'JSESSIONID': jsessionid,
                 'SESSION_COOKIE': sessionCookie,
-                # '__utmc': '121176714',
-                # 'Hm_lpvt_3db4e52bb5797afe0faaa2fde5c96ea4': '1516352533',
-                # '__utmb': '121176714.1.10.1516445514',
-                # '__utmt': '1'
             }
 
             headers = {
@@ -86,21 +76,8 @@ with open(filename) as csv_file:
                 # 'X-Requested-With': 'XMLHttpRequest',
                 #  'Connection': 'keep-alive'
             }
-            # print(len(row[8].split("-")))
-            # print(row[8].split("-"))
-            # print(len(row[8].split("-")))
-            # s = requests.session()
-            # s.headers = headers
-            # s.cookies = cookies
             request = requests.post('https://www.sui.com/tally/transfer.rmi',
                                     data=postdata,
                                     headers=headers,
                                     cookies=cookies)
             print('request.text:', request.text)
-            # curltext = " curl \"https://www.sui.com/tally/transfer.rmi\""
-            # curltext += " -H \"Content-Type: application/x-www-form-urlencoded; charset=UTF-8\""
-            # curltext += "  -H \"Cookie:    JSESSIONID=6D61B78E15F71CCE264953D3A2680E07;"
-            # curltext += " SESSION_COOKIE=14ef1450ea7d6f4f82ef548c622dde6d\""
-            # curltext += "  --data \"";
-            # curltext += parse.urlencode(postdata) + "\"";
-            # print(curltext)
